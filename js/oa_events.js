c7e9a065ee10cf8a690e6e738825f0a3de942d37
mpotter <mpotter@phase2technology.com>
2013-11-25 17:06:42 +0000

    Auto update End Date

--- js/oa_events.js
+++ js/oa_events.js
@@ -13,6 +13,20 @@
         $required = $required.detach();
         $('.node-oa_event-form .fieldset-legend').append($required[0]);
       }
+
+      // update the end date when the start date is changed
+      var $start_date = $('#edit-field-oa-date-und-0-value-datepicker-popup-0');
+      var $end_date   = $('#edit-field-oa-date-und-0-value2-datepicker-popup-0');
+      if ($start_date.length && $end_date.length) {
+        $start_date.data('oldval', $start_date.val());
+        $start_date.change(function() {
+          // only change end date if it hasn't already changed
+          if ($start_date.data('oldval') == $end_date.val()) {
+            $end_date.val($(this).val());
+          }
+          $start_date.data('oldval', $start_date.val());
+        });
+      }
     }
   }
 
